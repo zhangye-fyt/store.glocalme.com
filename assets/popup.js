f493874507b00514412c75a42f2f1e8a1a05c78b
zhangye-fyt <1595724547@qq.com>
2025-11-11 20:24:01 +0800

    all

--- assets/popup.js
+++ assets/popup.js
@@ -439,9 +439,9 @@ jQuery(document).ready(function($) {
 			*/
 			applyAnimation: function( el, name, callback ) {
 				// var popup = $( el ).closest( '.popup' );
-				console.log("111111111")
+				// console.log("111111111")
 				var popup = $(el).hasClass('popup') ? $(el) : $(el).closest('.popup');
-				console.log("popup",popup)
+				// console.log("popup",popup)
 
 				// if (!popup.length) {
 				// 	popup = $(el).prev('.popup');
